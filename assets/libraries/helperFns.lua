daee358c3c2646565fda236f19157ab2e9f853ed
Alex Nussel <alex@redoxengine.com>
2020-04-25 15:42:41 -0500

    draw inGame Table

--- assets/libraries/helperFns.lua
+++ assets/libraries/helperFns.lua
@@ -5,3 +5,40 @@ function table.shallow_copy(t)
     end
     return t2
 end
+
+function table.find(f, l) -- find element v of l satisfying f(v)
+  for _, v in ipairs(l) do
+    if f(v) then
+      return v
+    end
+  end
+  return nil
+end
+
+function splitString (inputstr, sep)
+  if sep == nil then
+    sep = "%s"
+  end
+  local t={}
+  for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+    table.insert(t, str)
+  end
+  return t
+end
+
+function table.generateValueIndex(index, inputTable)
+  local newTable = {}
+  local pathTable = splitString(index, '.')
+  for _, v in ipairs(inputTable) do
+    local property = v[pathTable[1]]
+    for i = 2, #pathTable do
+      local nextPath = pathTable[i]
+      property = property[nextPath]
+    end
+    if (newTable[property] == nil) then
+      newTable[property] = {}
+    end
+    table.insert(newTable[property], v)
+  end
+  return newTable
+end
--- assets/pages/inGame.lua
+++ assets/pages/inGame.lua
@@ -1,3 +1,29 @@
+local iffy = require 'assets/libraries/iffy'
+iffy.newAtlas('assets/sprites/playingCards.png')
+iffy.newAtlas('assets/sprites/RplayingCardBacks.png')
+
 local inGame = {}
+
+function inGame:draw()
+    local handLayer = drawLayers.layers[4]
+    local playerHandIndex = table.generateValueIndex('properties.player', handLayer.objects)
+    for _,info in ipairs(gameState.tableOrder) do
+        local player = gameState[info]
+        local cardNumb = 1
+        local cardPositions = playerHandIndex[info]
+        for _,card in ipairs(player) do
+            local cardName = ''
+            local thisCardPos = cardPositions[cardNumb]
+            if (info == 'myPlayer') then
+                cardName = 'card'..card[1]..'.png'
+            else
+                cardName = 'cardBack_r.png'
+            end
+            iffy.drawSprite(cardName, thisCardPos.x,
+                thisCardPos.y, thisCardPos.rotation * math.pi/180, initVars.scaleCards)
+            cardNumb = cardNumb + 1
+        end
+    end
+end
 
 return inGame
